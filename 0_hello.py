4752fdf3abeb97689c8fd2cfec9267145246c5a1
GoGoComputer <vividsaebom@gmail.com>
2024-01-16 15:39:11 +0900

    [update] new hello

---
+++ 0_hello.py
@@ -0,0 +1,44 @@
+print ("hello world")
+
+
+# 1. Install Python3: https://www.python.org/downloads/
+# 2. vscode download: https://code.visualstudio.com/
+# 3. vscode extension: Python3
+# 4. vscode extension: Code Runner
+
+# crate a new venv
+    # terminal: python3 -m venv myvenv # my env name: myvenv
+    # activate myvenv
+    # terminal: source myvenv/bin/activate
+    
+    
+    
+    # will deactivate the virtual environment
+    # terminal: deactivate
+
+
+# create a new file: 0_hello.py
+    # print ("hello world") and save file
+    # check python3 version terminal: python3 --version
+    # teminal: sudo apt install
+    # teminal: cd to the file path in terminal
+    # Run Python File in VSCODE  (0_hello.py)
+    
+# open ai package
+    # 1. activate myvenv
+    # terminal: pip3 install openai
+    
+# openai API call
+    # 1. make file .env
+    # 2. add API key to .env file
+    # 3. in .env file write: OPENAI_API_KEY="sk-xxxxxxx"
+    
+# gitignore [very very important!!!]
+    # 1. make file .gitignore
+    # 2. add myvenv to .gitignore file
+    # 3. add .env to .gitignore file
+    # 4. add myvenv to .gitignore file
+    
+    
+    
+    
